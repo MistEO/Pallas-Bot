2ab8f0a399af406efbf6bb8bc0220abcd33452fe
mole828 <mrx800@163.com>
2021-11-08 22:38:50 +0800

    仍然没有理解导包

--- bot/plugins/sese.py
+++ bot/plugins/sese.py
@@ -1,16 +1,17 @@
 
 from nonebot import on_message
+from nonebot.adapters.cqhttp import MessageSegment
 from nonebot.rule import startswith
 from nonebot.typing import T_State
-from nonebot.adapters import Bot, Event
-
-import json
+from nonebot.adapters import Bot, Event, Message
 
 tu = on_message(startswith("兔兔"))
 
-
 @tu.handle()
 async def handle_first_receive(bot: Bot, event: Event, state: T_State):
-    map=event.dict()
+
     #print(event.dict())
-    await tu.finish(f"str(event.dict()): {str(map)}")
+    m:MessageSegment=MessageSegment(type='image',data={'file':'http://www.moles.top/mfs/get/617fba373de23d49a3525139'})
+
+    await tu.finish(m)
+
