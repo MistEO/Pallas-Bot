432d46fdd1e76355426b20b876626f9d9ff52891
LoveLoliii <loli@loli.monster>
2021-11-15 22:57:16 +0800

    学会了dict from 鼹鼠

--- bot/plugins/anti-setu/__init__.py
+++ bot/plugins/anti-setu/__init__.py
@@ -9,19 +9,10 @@ from nonebot.adapters import Bot, Event
 hello = on_message()
 @hello.handle()
 async def handle_first_receive(bot: Bot, event: Event, state: T_State):
-    msg = event.get_message()
-    plainText = event.get_plaintext()
-    msgStr = str(msg)
-    msgStr = msgStr.replace(plainText, "")
-    msgStr = msgStr.replace("]", "]::")
-    cqList = msgStr.split("::")
-    for cq in cqList:
-        cqPropertyList = cq.replace("[", "").replace("]", "").split(",")
-        for cqp in cqPropertyList:
-            # print(cqp)
-            if "url" in cqp:
-                picLink = cqp.replace("url=", "")
-                # TODO baidu api
-                # doImgCheck()    
-        # print(cq)
+    print(event.dict())
+    if event.dict()['message_type'] == 'group':
+        for msg in event.dict()['message']:
+            if(msg['type'] == 'image'):
+                # print(msg['data']['url'])
+                return
     #await hello.send(msgStr)
