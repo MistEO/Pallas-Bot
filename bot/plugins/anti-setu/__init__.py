8d59d58fd35125c8a386d5568734b3e03b67cf2f
LoveLoliii <loli@loli.monster>
2021-11-17 11:21:05 +0800

    done

--- bot/plugins/anti-setu/__init__.py
+++ bot/plugins/anti-setu/__init__.py
@@ -10,12 +10,18 @@ from nonebot.rule import startswith
 from nonebot.rule import regex
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
+from .config import Config
+import nonebot
+
+global_config = nonebot.get_driver().config
+plugin_config = Config(**global_config.dict())
 
 door = False
-hello = on_message(startswith("x"))
+hello = on_message(block=False)
 @hello.handle()
 async def handle_first_receive(bot: Bot, event: Event, state: T_State):
     # print(event.dict())
+    global door
     print("in setu")
     print("door is "+str(door))
     if event.dict()['message_type'] == 'group':
@@ -23,21 +29,22 @@ async def handle_first_receive(bot: Bot, event: Event, state: T_State):
             if(msg['type'] == 'image') & door:
                 # print(msg['data']['url'])
                 reply = doImgCheck(str(msg['data']['url']))
-                if (reply != "") & door:
+                if (reply != "")&(reply != None) & door:
                     await hello.send(str(reply)) 
     # await hello.send(msgStr)
     return False
 
-switch = on_command("色图检测", rule=to_me(), priority=1)
+switch = on_command("色图检测", rule=to_me(), priority=5)
 @switch.handle()
 async def handle__first_receive(bot: Bot, event: Event, state: T_State):
-    print("in setu check")
+    # print("in setu check")
     args = str(event.get_message()).strip()
     if args:
         state["param"] = args
 
 @switch.got("param", prompt="开启或关闭")
 async def handle_param(bot: Bot, event: Event, state: T_State):
+    global door
     param = state["param"]
     if param not in ["开启", "关闭"]:
         await switch.reject("参数错误！")
@@ -48,10 +55,10 @@ async def handle_param(bot: Bot, event: Event, state: T_State):
     await switch.finish("已"+str(param))
 
 def doImgCheck(picUrl: str):
-    print("in img check function")
+    # print("in img check function")
     request_url = "https://aip.baidubce.com/rest/2.0/solution/v1/img_censor/v2/user_defined"
     params = {"imgUrl":picUrl}
-    access_token = 'ak'
+    access_token = plugin_config.baidu_api_ak
     request_url = request_url + "?access_token=" + access_token
     headers = {'content-type': 'application/x-www-form-urlencoded'}
     response = requests.post(request_url, data=params, headers=headers)
