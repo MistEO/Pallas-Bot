7527c1b8e9dacb666ca9975f33166b78555deb6d
mole828 <mrx800@163.com>
2021-11-17 22:14:07 +0800

    list[pic]用法的学习

--- tools/pixiv/pixiv.py
+++ tools/pixiv/pixiv.py
@@ -6,7 +6,7 @@ class pic:
     pic: str #url
     artwork: str #url
 
-def a60()->pic:
+def a60()->list[pic]:
     url = "http://a60.one:404/"
     res = json.loads(requests.get(url).text)
     print(res)
@@ -14,31 +14,29 @@ def a60()->pic:
     p.id = int(res['pic'].split('_')[0])
     p.pic = res['url']
     p.artwork=f'https://www.pixiv.net/artworks/{p.id}'
-    return p
-
-def _elUrl(pir):
-    meta_pages = pir['meta_pages']
-    if len(meta_pages) == 0:
-        url = pir['meta_single_page']['original_image_url']
-    else:
-        url = meta_pages[0]['image_urls']['original']
-    return url.replace('i.pximg.net', 'proxy-jp1.pixivel.moe')
+    return [p]
 
 
-pics = []
-def pixivel(page=0)->pic:
-    if len(pics)>0:
-        return pics.pop()
+
+def pixivel(page=0)->list[pic]:
 
     url = "https://api-jp1.pixivel.moe/pixiv?type=illust_recommended&page={}".format(page)
     res = json.loads(requests.get(url).text)
+    pics = []
+    def _elUrl(pir):
+        meta_pages = pir['meta_pages']
+        if len(meta_pages) == 0:
+            url = pir['meta_single_page']['original_image_url']
+        else:
+            url = meta_pages[0]['image_urls']['original']
+        return url.replace('i.pximg.net', 'proxy-jp1.pixivel.moe')
     for pir in res['illusts']:
         p = pic()
         p.id = pir['id']
         p.pic = _elUrl(pir)
         p.artwork = f'https://www.pixiv.net/artworks/{p.id}'
         pics.append(p)
-    return pixivel()
+    return pics
 
 if __name__=='__main__':
     print(pixivel().__dict__)
