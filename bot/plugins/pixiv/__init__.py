6ec8074e5c69d709be698fa87680a7fea650b971
mole828 <mrx800@163.com>
2021-11-21 15:00:32 +0800

    删除不需要的逻辑

--- bot/plugins/pixiv/__init__.py
+++ bot/plugins/pixiv/__init__.py
@@ -4,7 +4,7 @@ from nonebot.rule import startswith, to_me
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
 
-from tools.pixiv.keeper import keeper
+from tools.pixiv.pixiv import a60
 
 tu = on_command(cmd="涩涩",
                 priority=10,
@@ -13,17 +13,9 @@ tu = on_command(cmd="涩涩",
 keepers = {}
 
 
-def groupKeeper(group: int) -> keeper:
-    kp = keepers.get(group)
-    if kp is None:
-        kp = keeper(group=group)
-        keepers[group] = kp
-    return kp
-
-
 @tu.handle()
 async def handle_first_receive(bot: Bot, event: GroupMessageEvent, state: T_State):
-    p = await groupKeeper(event.group_id).random()
+    p = (await a60())[0]
     url = f'https://www.pixiv.net/artworks/{p.id}'
     msg: Message = MessageSegment.text(url) + MessageSegment.image(file=p.pic)
     await tu.finish(msg)
