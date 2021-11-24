827afd77b7650580b71d3e157b1166ff7bc63e7e
MistEO <mistereo@hotmail.com>
2021-11-24 14:57:15 +0800

    改改欢迎文案

--- bot/plugins/greeting/__init__.py
+++ bot/plugins/greeting/__init__.py
@@ -71,5 +71,5 @@ async def handle_first_receive(bot: Bot, event: Event, state: T_State):
             msg = '我是来自米诺斯的祭司帕拉斯，会在罗德岛休息一段时间......虽然这么说，我渴望以美酒和戏剧被招待，更渴望走向战场。'
         else:
             msg: Message = MessageSegment.at(event.dict()['user_id']) + MessageSegment.text(
-                '博士，欢迎加入这盛大的祭典，高歌吧！为这片大地的改变而奋斗。')
+                '博士，欢迎加入这盛大的庆典，我是来自米诺斯的祭司帕拉斯......要来一杯美酒么？')
         await all_notice.finish(msg)
