c17533d6e09c9e788de1c482cfcd1a034623f84c
MistEO <mistereo@hotmail.com>
2021-11-28 06:34:48 +0800

    新增推送微博功能

--- bot/plugins/push/README.md
+++ bot/plugins/push/README.md
@@ -2,6 +2,13 @@
 
 ## Todo
 
-- [ ] bili直播
-- [ ] 微博
+- [x] bili直播
+- [x] 微博
 - [ ] github版本发布
+
+## 致谢
+
+- 感谢[鼹鼠](https://github.com/mole828)的bilibili Api
+- 感谢[兔妈](https://github.com/vivien8261)的微博爬虫
+
+（总之没有一个是我自己写的2333
--- bot/plugins/push/__init__.py
+++ bot/plugins/push/__init__.py
@@ -30,3 +30,33 @@ async def push_bili():
                     'group_id': group})
         
         bili_status[item] = now
+
+from .weibo import Weibo
+
+weibo_list = []
+for weibo_id in plugin_config.weibo_id:
+    weibo_list.append(Weibo(weibo_id))
+
+weibo_status = {}
+
+@sched.scheduled_job('interval', seconds=5)
+async def push_weibo():
+    for wb in weibo_list:
+        pre = weibo_status.get(wb, False)
+        now = wb.requests_content(0, only_id=True)
+        if pre and pre != now:
+        # if True:
+            result, detail_url, pics_list = wb.requests_content(0)
+            msg: Message = MessageSegment.text(detail_url + '\n') \
+                + MessageSegment.text(result)
+            if pics_list:
+                for pic in pics_list:
+                    msg += MessageSegment.image(pic)
+            
+            for group in plugin_config.weibo_push_groups:
+                await get_bot().call_api('send_group_msg', **{
+                    'message': msg,
+                    'group_id': group})
+            
+        weibo_status[wb] = now
+
