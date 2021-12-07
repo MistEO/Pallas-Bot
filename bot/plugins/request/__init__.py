834b21a4c8d7732ea8edb0eec571d30991144446
yuan3060 <1846865993@qq.com>
2021-12-07 20:42:30 +0800

    增加自动加入 群聊 私聊 功能

---
+++ bot/plugins/request/__init__.py
@@ -0,0 +1,31 @@
+import nonebot
+from nonebot import on_command, on_request, on_notice, get_driver
+from nonebot.adapters.cqhttp import MessageSegment, Message, FriendRequestEvent, GroupRequestEvent, Bot
+from nonebot.rule import keyword, startswith, to_me
+from nonebot.typing import T_State
+from typing import Union
+
+request_cmd = on_request(
+    priority=14,
+    block=True,
+)
+
+
+@request_cmd.handle()
+async def handle_request(bot: Bot, event: Union[GroupRequestEvent, FriendRequestEvent], state: T_State):
+    if isinstance(event, GroupRequestEvent):
+        await bot.set_group_add_request(
+            flag=event.flag,
+            sub_type=event.sub_type,
+            approve=True,
+            reason=""
+        )
+        nonebot.logger.info("同意加入群聊: {}", event. group_id)
+    elif isinstance(event, FriendRequestEvent):
+        nonebot.logger.info("属于私有")
+        await bot.set_friend_add_request(
+            flag=event.flag,
+            approve=True,
+            remark=""
+        )
+        nonebot.logger.info("同意添加好友: {}", event.user_id)
