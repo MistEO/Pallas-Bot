fbfc2a863fe688e24cfd138ac438f45f97c90c08
MistEO <mistereo@hotmail.com>
2022-02-09 01:53:17 +0800

    refactor.完成牛牛复读基本功能的重写

--- bot/plugins/repeater/model.py
+++ bot/plugins/repeater/model.py
@@ -1,10 +1,14 @@
 from typing import List, Optional, Union
 from functools import cached_property
 from dataclasses import dataclass
+from collections import defaultdict
 
 import jieba_fast.analyse
 import pypinyin
 import pymongo
+import time
+import random
+import re
 
 from nonebot.adapters import Event
 
@@ -15,7 +19,7 @@ mongo_db = mongo_client['PallasBot']
 mongo_message = mongo_db['message']
 mongo_context = mongo_db['context']
 
-reply_time_dict = {}
+reply_dict = {}
 
 
 @dataclass
@@ -30,18 +34,21 @@ class ChatData:
 
     @cached_property
     def is_plain_text(self) -> bool:
-        if '[CQ:' not in self.raw_message:
-            return True
-        else:
-            return False
+        return '[CQ:' not in self.raw_message
+
+    @cached_property
+    def is_image(self) -> bool:
+        return '[CQ:image,' in self.raw_message or '[CQ:face,' in self.raw_message
 
     @cached_property
     def keywords(self) -> List[str]:
         if not self.is_plain_text:
             return []
 
-        return jieba_fast.analyse.extract_tags(
+        result = jieba_fast.analyse.extract_tags(
             self.plain_text, topK=ChatData._keywords_size)
+        result.sort()
+        return result
 
     @cached_property
     def keywords_pinyin(self) -> List[str]:
@@ -67,7 +74,9 @@ class Chat:
             self.chat_data = ChatData(
                 group_id=event_dict['group_id'],
                 user_id=event_dict['user_id'],
-                raw_message=event_dict['raw_message'],
+                # 删除图片子类型字段，同一张图子类型经常不一样，影响判断
+                raw_message=re.sub(',subType=\d+', '',
+                                   event_dict['raw_message']),
                 plain_text=data.get_plaintext(),
                 time=event_dict['time']
             )
@@ -99,7 +108,32 @@ class Chat:
         '''
         回复这句话，可能会分多次回复（所以是List），也可能不回复
         '''
-        pass
+
+        if self.chat_data.group_id in reply_dict:
+            latest_reply = reply_dict[self.chat_data.group_id]
+            # 限制发音频率，最多 5 秒一次
+            if self.chat_data.time - latest_reply['time'] < 5:
+                return None
+            # # 不要一直回复同一个内容
+            # if self.chat_data.raw_message == latest_reply['pre_msg']:
+            #     return None
+            # 有人复读了牛牛的回复，不继续回复
+            if self.chat_data.raw_message == latest_reply['cur_msg']:
+                return None
+
+        # 不回复太短的对话，大部分是“？”、“草”
+        if self.chat_data.is_plain_text and len(self.chat_data.plain_text) < 2:
+            return None
+
+        result = self._context_find()
+
+        if result:
+            reply_dict[self.chat_data.group_id] = {
+                'time': (int)(time.time()),
+                'cur_msg': result[-1],
+                'pre_msg': self.chat_data.raw_message
+            }
+        return result
 
     def _message_insert(self):
         mongo_message.insert_one({
@@ -118,6 +152,10 @@ class Chat:
 
         # 在复读，不学
         if pre_msg['raw_message'] == self.chat_data.raw_message:
+            return
+
+        # 回复别人的，不学
+        if '[CQ:reply,' in self.chat_data.raw_message:
             return
 
         # # 如果有反过来的，直接退出，说明可能是两句话在轮流复读。只取正向的（先达到阈值的）
@@ -159,14 +197,95 @@ class Chat:
             nested = 'cur_raw_msg_options.' + self.chat_data.raw_message
             set_value['$inc'][nested] = 1
 
-        print(set_value)
         mongo_context.update_one(update_key, set_value, upsert=True)
+
+    def _context_find(self) -> Optional[List[str]]:
+
+        rand = random.randint(0, 100)
+        if rand < 30:
+            count_thres = Chat._count_threshold - 1
+        elif rand < 60:
+            count_thres = Chat._count_threshold
+        else:
+            count_thres = Chat._count_threshold + 1
+
+        # hist_msg = mongo_message.find({
+        #     'group_id': self.chat_data.group_id,
+        # }, sort=[('time', pymongo.DESCENDING)]).limit(count_thres)
+
+        # # 复读！
+        # if hist_msg:
+        #     is_repeat = True
+        #     for item in hist_msg:
+        #         if self.chat_data.raw_message != item.raw_message:
+        #             is_repeat = False
+        #             break
+        #     if is_repeat:
+        #         return [self.chat_data.raw_message, ]
+
+        if self.chat_data.is_plain_text:
+            all_answers = mongo_context.find({
+                'pre_keywords': self.chat_data.keywords,
+                'count': {'$gt': count_thres}
+            })
+        elif self.chat_data.is_image:
+            all_answers = mongo_context.find({
+                'pre_raw_msg': self.chat_data.raw_message,
+                'count': {'$gt': count_thres},
+                'cur_raw_msg': {'$regex': '^\[CQ:'}
+            })
+        else:
+            all_answers = mongo_context.find({
+                'pre_raw_msg': self.chat_data.raw_message,
+                'count': {'$gt': count_thres}
+            })
+        filtered_answers = []
+        answers_count = defaultdict(int)
+        for answer in all_answers:
+            if answer['group_id'] == self.chat_data.group_id:
+                filtered_answers.append(answer)
+                continue
+            elif answer['cur_is_plain_text']:
+                answers_count[answer['cur_keywords']] += 1
+                cur_count = answers_count[answer['cur_keywords']]
+            elif '[CQ:at,' in answer['cur_raw_msg']:    # 别的群的 at, 过滤掉
+                continue
+            else:
+                answers_count[answer['cur_raw_msg']] += 1
+                cur_count = answers_count[answer['cur_raw_msg']]
+
+            if cur_count == count_thres:    # 有这么多个群都有相同的回复，就作为全局回复
+                filtered_answers.append(answer)
+
+        if not len(filtered_answers):
+            return None
+
+        # count 越大的结果，回复的概率越大
+        count_seg = []
+        count_sum = 0
+        for answer in filtered_answers:
+            count_sum += answer['count'] ** 2
+            count_seg.append(count_sum)
+        rand_value = random.randint(0, count_sum)
+        rand_index = 0
+        for index in range(len(count_seg)):
+            if rand_value < count_seg[index]:
+                rand_index = index
+                break
+
+        final_answer = filtered_answers[rand_index]
+        if final_answer['cur_is_plain_text']:
+            answer_str = random.choice(
+                list(final_answer['cur_raw_msg_options']))
+        else:
+            answer_str = final_answer['cur_raw_msg']
+
+        if 0 < answer_str.count('，') <= 3 and random.randint(1, 10) < 5:
+            return answer_str.split('，')
+        return [answer_str, ]
 
 
 if __name__ == '__main__':
-
-    import time
-
     test_data: ChatData = ChatData(
         group_id=1234567,
         user_id=1111111,
@@ -176,9 +295,11 @@ if __name__ == '__main__':
     )
 
     test_chat: Chat = Chat(test_data)
+
+    print(test_chat.answer())
     test_chat.learn()
 
-    test_data: ChatData = ChatData(
+    test_answer_data: ChatData = ChatData(
         group_id=1234567,
         user_id=1111111,
         raw_message='别烦，我学MySql啊',
@@ -186,5 +307,5 @@ if __name__ == '__main__':
         time=time.time()
     )
 
-    test_chat: Chat = Chat(test_data)
-    test_chat.learn()
+    test_answer: Chat = Chat(test_answer_data)
+    test_answer.learn()
