0590831181aabfd0fb61c42185108e78d887242b
MistEO <mistereo@hotmail.com>
2022-05-04 04:39:18 +0800

    feat.复刻牛牛自动加群功能

--- src/plugins/request/__init__.py
+++ src/plugins/request/__init__.py
@@ -5,14 +5,29 @@ from nonebot.rule import keyword, startswith, to_me
 from nonebot.typing import T_State
 from typing import Union
 
+import pymongo
+
 request_cmd = on_request(
     priority=14,
     block=True,
 )
 
+mongo_client = pymongo.MongoClient('127.0.0.1', 27017, w=0)
+mongo_db = mongo_client['PallasBot']
+request_mongo = mongo_db['request']
+
 
 @request_cmd.handle()
 async def handle_request(bot: Bot, event: Union[GroupRequestEvent, FriendRequestEvent], state: T_State):
+    data = request_mongo.find_one({
+        "type": "auto_accept"
+    })
+    if data is None or 'accounts' not in data:
+        return
+
+    if event.self_id not in data['accounts']:
+        return
+
     if isinstance(event, GroupRequestEvent):
         await bot.set_group_add_request(
             flag=event.flag,
