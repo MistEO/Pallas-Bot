ae42cf4b3756674e30c74fb77313263bdc3a1445
MistEO <mistereo@hotmail.com>
2022-05-09 01:17:34 +0800

    fix.修复account设置查询错误的问题

--- src/common/config/__init__.py
+++ src/common/config/__init__.py
@@ -9,19 +9,19 @@ mongo_client = pymongo.MongoClient('127.0.0.1', 27017, w=0)
 mongo_db = mongo_client['PallasBot']
 config_mongo = mongo_db['config']
 config_mongo.create_index(name='accounts_index',
-                            keys=[('account', pymongo.HASHED)])
+                          keys=[('account', pymongo.HASHED)])
 
 
 @dataclass
 class BotConfig:
     def __init__(self, bot_id: int) -> None:
         self.bot_id = bot_id
-        self._find_key = {
+        self._mongo_find_key = {
             'account': bot_id
         }
 
     def _find_key(self, key: str) -> Any:
-        info = config_mongo.find_one(self._find_key)
+        info = config_mongo.find_one(self._mongo_find_key)
         if info and key in info:
             return info[key]
         else:
