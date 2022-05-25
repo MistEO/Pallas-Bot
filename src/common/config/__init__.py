773a7a2b3970d5306ba804d831f721d579925551
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-05-25 16:06:56 +0800

    Make config_mongo load lazily

--- src/common/config/__init__.py
+++ src/common/config/__init__.py
@@ -1,17 +1,23 @@
 import pymongo
+from pymongo.collection import Collection
 
 from dataclasses import dataclass
-from typing import Any
-
-
-mongo_client = pymongo.MongoClient('127.0.0.1', 27017, w=0)
-mongo_db = mongo_client['PallasBot']
-config_mongo = mongo_db['config']
-config_mongo.create_index(name='accounts_index',
-                          keys=[('account', pymongo.HASHED)])
+from typing import Any, Optional
 
 
 class BotConfig:
+    __config_mongo: Optional[Collection] = None
+
+    @classmethod
+    def _get_config_mongo(cls) -> Collection:
+        if cls.__config_mongo is None:
+            mongo_client = pymongo.MongoClient('127.0.0.1', 27017, w=0)
+            mongo_db = mongo_client['PallasBot']
+            cls.__config_mongo = mongo_db['config']
+            cls.__config_mongo.create_index(name='accounts_index',
+                                            keys=[('account', pymongo.HASHED)])
+        return cls.__config_mongo
+
     def __init__(self, bot_id: int) -> None:
         self.bot_id = bot_id
         self._mongo_find_key = {
@@ -19,7 +25,7 @@ class BotConfig:
         }
 
     def _find_key(self, key: str) -> Any:
-        info = config_mongo.find_one(self._mongo_find_key)
+        info = self._get_config_mongo().find_one(self._mongo_find_key)
         if info and key in info:
             return info[key]
         else:
@@ -50,7 +56,7 @@ class BotConfig:
         '''
         添加管理员
         '''
-        config_mongo.update_one(
+        self._get_config_mongo().update_one(
             self._mongo_find_key,
             {'$push': {'admins': user_id}},
             upsert=True
