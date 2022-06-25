b187874a0a7debbe6a18dbb0b6164e13c8348e88
MistEO <mistereo@hotmail.com>
2022-06-26 02:02:30 +0800

    feat: 号主邀请牛牛加群会自动同意了

--- src/plugins/request/__init__.py
+++ src/plugins/request/__init__.py
@@ -1,23 +1,19 @@
 import nonebot
 from nonebot import on_command, on_request, on_notice, get_driver
 from nonebot.adapters.onebot.v11 import MessageSegment, Message, FriendRequestEvent, GroupRequestEvent, Bot
-from nonebot.rule import keyword, startswith, to_me
 from nonebot.typing import T_State
-from typing import Union
 
 from src.common.config import BotConfig
 
 request_cmd = on_request(
     priority=14,
-    block=True,
+    block=False,
 )
 
 
 @request_cmd.handle()
-async def handle_request(bot: Bot, event: Union[GroupRequestEvent, FriendRequestEvent], state: T_State):
+async def handle_request(bot: Bot, event: GroupRequestEvent, state: T_State):
 
-    if event.sub_type != 'invite':
-        return
-
-    if BotConfig(event.self_id).auto_accept():
-        await event.approve(bot)
+    if event.sub_type == 'invite':
+        if BotConfig(event.self_id).auto_accept() or BotConfig(event.self_id).is_admin(event.user_id):
+            await event.approve(bot)
