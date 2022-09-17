61dce6b7800b2850dd363c47baf81da61bd1c34e
MistEO <mistereo@hotmail.com>
2022-09-18 04:28:22 +0800

    feat: 牛牛做梦支持小作文

--- src/plugins/dream/__init__.py
+++ src/plugins/dream/__init__.py
@@ -1,12 +1,14 @@
 import asyncio
 import time
 import random
+import re
 from typing import Union, List, Optional
 from urllib import response
 from asyncer import asyncify
 
 import wenxin_api
 from wenxin_api.tasks.text_to_image import TextToImage
+from wenxin_api.tasks.composition import Composition
 
 from nonebot import on_message
 from nonebot.typing import T_State
@@ -34,19 +36,52 @@ dream_msg = on_message(
     permission=permission.GROUP
 )
 
+wenxin_api.ak = wenxin_ak
+wenxin_api.sk = wenxin_sk
+
 
 def gen_images(text: str) -> Optional[List[str]]:
-    wenxin_api.ak = wenxin_ak
-    wenxin_api.sk = wenxin_sk
     input_dict = {
         "text": text,
         "style": random.choice(['水彩', '油画', '粉笔画', '卡通', '蜡笔画', '儿童画'])
     }
     response = TextToImage.create(**input_dict)
-    if 'imgUrls' in response:
-        return response['imgUrls']
-    else:
+    if 'imgUrls' not in response:
         return None
+    return response['imgUrls']
+
+
+def gen_text(text: str) -> Optional[List[str]]:
+    input_dict = {
+        "text": f"作文题目：{text}\n正文：",
+        "seq_len": 256,
+        "topp": 0.9,
+        "penalty_score": 1.2,
+        "min_dec_len": 64,
+        "is_unidirectional": 0,
+        "task_prompt": "zuowen"
+    }
+    response = Composition.create(**input_dict)
+    if 'result' not in response:
+        return None
+
+    result = response['result']
+    pre_pos = 0
+    in_quotes = False
+    for pos in range(len(result)):
+        if result[pos] == '“':
+            in_quotes = True
+        elif result[pos] == '”':
+            in_quotes = False
+        if in_quotes:
+            continue
+
+        if result[pos] in ['。', '！', '？', '；', ' ', '：', '~', '…']:
+            yield result[pre_pos:pos+1].strip()
+            pre_pos = pos + 1
+
+    yield result[pre_pos:].strip() + '……'
+    yield 'Zzz……'
 
 
 @dream_msg.handle()
@@ -62,14 +97,26 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     config.refresh_cooldown('dream')
 
     await dream_msg.send('Zzz……')
-    start = time.time()
-    images_list = await asyncify(gen_images)(context)
-    if not images_list:
-        return
 
-    duration = time.time() - start
-    for image in images_list:
-        msg: Message = MessageSegment.image(image)
-        await dream_msg.send(msg)
-        duration /= 2
-        await asyncio.sleep(duration)
+    send_images = random.random() < 0.25
+    if send_images:
+        start = time.time()
+        images_list = await asyncify(gen_images)(context)
+        if not images_list:
+            return
+
+        duration = time.time() - start
+        for image in images_list:
+            msg: Message = MessageSegment.image(image)
+            await dream_msg.send(msg)
+            duration /= 2
+            await asyncio.sleep(duration)
+
+    else:
+        text_list = await asyncify(gen_text)(context)
+        if not text_list:
+            return
+
+        for text in text_list:
+            await dream_msg.send(text)
+            await asyncio.sleep(random.randint(2, 5))
