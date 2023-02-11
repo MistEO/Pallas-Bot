ddb56b6eadadf8bc6bd6bd59aa1be8dd2108b128
MistEO <mistereo@hotmail.com>
2023-02-11 16:46:18 +0000

    perf: 为牛牛唱歌吃GPU的部分加个锁。并添加异步操作

--- src/plugins/sing/__init__.py
+++ src/plugins/sing/__init__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from threading import Lock
+from asyncer import asyncify
 
 from nonebot import on_message
 from nonebot.typing import T_State
@@ -20,9 +21,11 @@ cmd = '牛牛唱歌'
 sing_msg = on_message(
     rule=startswith(cmd),
     priority=5,
-    block=True,
+    block=False,
     permission=permission.GROUP
 )
+
+inference_locker = Lock()
 
 
 @sing_msg.handle()
@@ -36,35 +39,40 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
         config.refresh_cooldown('sing', reset=True)
         await sing_msg.finish('我习惯了站着不动思考。有时候啊，也会被大家突然戳一戳，看看睡着了没有。')
 
+    # 下载 -> 切片 -> 人声分离 -> 音色转换（SVC） -> 混音
+    # 其他 人声分离和音色转换是吃 GPU 的，所以要加锁，不然显存不够用
+    await sing_msg.send('欢呼吧！')
+
     song_id = event.get_plaintext().replace(cmd, '').strip()
     # 从网易云下载
-    path = get_song_path(song_id)
+    path = await asyncify(get_song_path)(song_id)
     if not path:
         await failed()
 
     # 音频切片
-    slices_list = slice(path, Path('resource/sing/slices'), song_id)
+    slices_list = await asyncify(slice)(path, Path('resource/sing/slices'), song_id)
     if not slices_list:
         await failed()
 
-    # TODO: 做个对象池
     # TODO: 记录每个群上次唱到哪了
     # 人声分离
     idx = 0
     chunk = slices_list[idx]
-    separated = separate(chunk, Path('resource/sing/'))
+    with inference_locker:
+        separated = await asyncify(separate)(chunk, Path('resource/sing/'))
     if not separated:
         await failed()
 
     vocals, no_vocals = separated
 
-    # 人声音色转换（SVC）
-    svc = inference(vocals, Path('resource/sing/svc/'))
+    # 音色转换（SVC）
+    with inference_locker:
+        svc = await asyncify(inference)(vocals, Path('resource/sing/svc/'))
     if not svc:
         await failed()
 
-    # 混合
-    result = mix(svc, no_vocals, chunk.stem)
+    # 混合人声和伴奏
+    result = await asyncify(mix)(svc, no_vocals, chunk.stem)
     if not result:
         await failed()
 
