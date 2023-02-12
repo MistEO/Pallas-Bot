abb89eb008daa9b8059137d32415a3d464fa2ff9
MistEO <mistereo@hotmail.com>
2023-02-12 10:45:48 +0000

    feat: 重构唱歌模块以支持多发音人

--- src/plugins/sing/__init__.py
+++ src/plugins/sing/__init__.py
@@ -18,15 +18,57 @@ from .slicer import slice
 from .svc_inference import inference
 from .mixer import mix
 
-SING_CMD = '牛牛唱歌'
-SING_CONTINUE_CMDS = ['牛牛继续唱', '牛牛接着唱']
+
+# key 对应命令词，开头必须是人名
+# value 对应 resource/sing/models/ 下的文件夹名，以及生成的音频文件名
+SPEAKERS = {
+    "帕拉斯": "pallas",
+    "牛牛": "pallas",
+    "阿米娅": "amiya",
+    "兔兔": "amiya",
+    "安洁莉娜": "aglina",
+    "洁哥": "aglina",
+    "杰哥": "aglina",
+    "能天使": "angel",
+    "阿能": "angel",
+    "澄闪": "gdglow",
+    "粉猫猫": "gdglow",
+}
+SING_CMD = '唱歌'
+SING_CONTINUE_CMDS = ['继续唱', '接着唱']
 SING_COOLDOWN_KEY = 'sing'
 
 
-async def is_to_sing(bot: "Bot", event: "Event", state: T_State) -> bool:
+async def is_to_sing(bot: Bot, event: Event, state: T_State) -> bool:
     text = event.get_plaintext()
-    return (SING_CMD in text and text.replace(SING_CMD, '').strip().isdigit()
-            ) or text in SING_CONTINUE_CMDS
+    if not text:
+        return False
+
+    has_spk = False
+    for name, speaker in SPEAKERS.items():
+        if not text.startswith(name):
+            continue
+        text = text.replace(name, '').strip()
+        has_spk = True
+        state['speaker'] = speaker
+        break
+
+    if not has_spk:
+        return False
+
+    if text.startswith(SING_CMD):
+        song_id = text.replace(SING_CMD, '').strip()
+        if not song_id or not song_id.isdigit():
+            return False
+        state['song_id'] = song_id
+        state['chunk_index'] = 0
+        return True
+    elif text in SING_CONTINUE_CMDS and event.group_id in chunk_progess:
+        state['song_id'] = chunk_progess[event.group_id]['song_id']
+        state['chunk_index'] = chunk_progess[event.group_id]['chunk_index']
+        return True
+
+    return False
 
 sing_msg = on_message(
     rule=Rule(is_to_sing),
@@ -41,19 +83,6 @@ chunk_progess = {}
 
 @sing_msg.handle()
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
-
-    if SING_CMD in event.get_plaintext():
-        song_id = event.get_plaintext().replace(SING_CMD, '').strip()
-        chunk_index = 0
-    elif event.group_id in chunk_progess:
-        song_id = chunk_progess[event.group_id]['song_id']
-        chunk_index = chunk_progess[event.group_id]['chunk_index']
-    else:
-        return
-
-    if not song_id or not song_id.isdigit():
-        return
-
     config = BotConfig(event.self_id, event.group_id, cooldown=60)
     if not config.is_cooldown(SING_COOLDOWN_KEY):
         return
@@ -63,9 +92,13 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
         config.refresh_cooldown(SING_COOLDOWN_KEY, reset=True)
         await sing_msg.finish('我习惯了站着不动思考。有时候啊，也会被大家突然戳一戳，看看睡着了没有。')
 
+    await sing_msg.send('欢呼吧！')
+
+    speaker = state['speaker']
+    song_id = state['song_id']
+    chunk_index = state['chunk_index']
     # 下载 -> 切片 -> 人声分离 -> 音色转换（SVC） -> 混音
     # 其中 人声分离和音色转换是吃 GPU 的，所以要加锁，不然显存不够用
-    await sing_msg.send('欢呼吧！')
 
     # 从网易云下载
     origin = await asyncify(download)(song_id)
@@ -87,12 +120,12 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     vocals, no_vocals = separated
 
     # 音色转换（SVC）
-    svc = await asyncify(inference)(vocals, Path('resource/sing/svc'), locker=gpu_locker)
+    svc = await asyncify(inference)(vocals, Path('resource/sing/svc'), speaker=speaker, locker=gpu_locker)
     if not svc:
         await failed()
 
     # 混合人声和伴奏
-    result = await asyncify(mix)(svc, no_vocals, Path("resource/sing/mix"), chunk.stem)
+    result = await asyncify(mix)(svc, no_vocals, Path("resource/sing/mix"), svc.stem)
     if not result:
         await failed()
 
@@ -108,26 +141,42 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
 
 
 # 青春版唱歌（bushi
-async def message_equal(bot: "Bot", event: "Event", state: T_State) -> bool:
-    return event.raw_message in ['牛牛唱歌', '欢乐水牛']
+async def play_song(bot: Bot, event: Event, state: T_State) -> bool:
+    text = event.get_plaintext()
+    if not text or not text.endswith(SING_CMD):
+        return False
+
+    for name, speaker in SPEAKERS.items():
+        if not text.startswith(name):
+            continue
+        state['speaker'] = speaker
+        return True
+
+    return False
 
 
 play_cmd = on_message(
-    rule=Rule(message_equal),
+    rule=Rule(play_song),
     priority=13,
     block=False,
     permission=permission.GROUP)
 
 
-MUSIC_PATH = 'resource/music/'
 SONG_PATH = 'resource/sing/mix/'
+MUSIC_PATH = 'resource/music/'
 
 
-def get_random_music():
-    all_music = [MUSIC_PATH + s for s in os.listdir(MUSIC_PATH)]
-    all_music += [SONG_PATH +
-                  s for s in os.listdir(SONG_PATH) if '_chunk0' in s]
-    return random.choice(all_music)
+def get_random_song(speaker: str = ""):
+    all_song = []
+    if os.path.exists(SONG_PATH):
+        all_song = [SONG_PATH +
+                    s for s in os.listdir(SONG_PATH) if '_chunk0' in s and speaker in s]
+    if not all_song:
+        all_song = [MUSIC_PATH + s for s in os.listdir(MUSIC_PATH)]
+
+    if not all_song:
+        return None
+    return random.choice(all_song)
 
 
 @play_cmd.handle()
@@ -137,7 +186,10 @@ async def _(bot: Bot, event: Event, state: T_State):
         return
     config.refresh_cooldown('music')
 
-    rand_music = get_random_music()
+    speaker = state['speaker']
+    rand_music = get_random_song(speaker)
+    if not rand_music:
+        return
 
     if '_chunk0' in rand_music:
         song_id = Path(rand_music).stem.split('_')[0]
@@ -152,7 +204,8 @@ async def _(bot: Bot, event: Event, state: T_State):
 
 async def what_song(bot: "Bot", event: "Event", state: T_State) -> bool:
     text = event.get_plaintext()
-    return '牛牛' in text and ('什么歌' in text or '哪首歌' in text or '啥歌' in text)
+    return any([text.startswith(spk) for spk in SPEAKERS.keys()]) \
+        and any(key in text for key in ['什么歌', '哪首歌', '啥歌'])
 
 
 song_title_cmd = on_message(
--- src/plugins/sing/svc_inference.py
+++ src/plugins/sing/svc_inference.py
@@ -2,25 +2,41 @@ import os
 from threading import Lock
 from pathlib import Path
 
-SVC_MODEL = Path('resource/sing/models/G_60000.pth').absolute()
-SVC_CONFIG = Path('resource/sing/models/config.json').absolute()
 SVC_HUBERT = Path('resource/sing/models/hubert-soft-0d54a1f4.pt').absolute()
 SVC_MAIN = (Path(__file__).parent / 'so_vits_svc' /
             'inference_caller.py').absolute()
-SVC_SPEAKER = "pallas"
 SVC_SLICE_DB = -40
 SVC_OUPUT_FORMAT = 'flac'
 
+speaker_models = {}
 
-def inference(song_path: Path, output_dir: Path, key: int = 0, locker: Lock = Lock()):
+def inference(song_path: Path, output_dir: Path, key: int = 0, speaker: str = "pallas", locker: Lock = Lock()):
     # 这个库不知道咋集成，似乎可以转成 ONNX，但是我不会
     # 先用 cmd 凑合跑了
     # TODO: 使用 ONNX Runtime 重新集成
+
     result = output_dir / \
-        f'{song_path.parent.stem}_{key}key_{SVC_SPEAKER}.{SVC_OUPUT_FORMAT}'
+        f'{song_path.parent.stem}_{key}key_{speaker}.{SVC_OUPUT_FORMAT}'
 
     if not os.path.exists(result):
-        cmd = f'python {SVC_MAIN} {SVC_MODEL} {SVC_CONFIG} {SVC_HUBERT} {song_path.absolute()} {key} {SVC_SPEAKER} {SVC_SLICE_DB} {output_dir.absolute()} {SVC_OUPUT_FORMAT}'
+        global speaker_models
+
+        model = ""
+        if speaker not in speaker_models:
+            models_dir = Path(f'resource/sing/models/{speaker}/')
+            for m in os.listdir(models_dir):
+                if m.startswith('G_') and m.endswith('.pth'):
+                    speaker_models[speaker] = models_dir / m
+                    break
+        
+        model = speaker_models[speaker].absolute()
+        config = Path(f'resource/sing/models/{speaker}/config.json').absolute()
+
+        if not os.path.exists(model) or not os.path.exists(config) or not os.path.exists(SVC_HUBERT):
+            print("!!! Model or config not found !!!")
+            return None
+
+        cmd = f'python {SVC_MAIN} {model} {config} {SVC_HUBERT} {song_path.absolute()} {key} {speaker} {SVC_SLICE_DB} {output_dir.absolute()} {SVC_OUPUT_FORMAT}'
         with locker:
             print(cmd)
             os.system(cmd)
