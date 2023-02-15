df76d3a58fb2a3c7518f55cc57e25c37f3b99332
MistEO <mistereo@hotmail.com>
2023-02-15 14:32:55 +0800

    perf: SVC去除格式限制，人声分离改用mp3格式以减小磁盘占用

--- src/plugins/sing/separater.py
+++ src/plugins/sing/separater.py
@@ -7,12 +7,12 @@ def separate(song_path: Path, output_dir: Path, locker: Lock = Lock()):
     MODEL = 'hdemucs_mmi'
     STEM = song_path.stem
 
-    vocals = output_dir / MODEL / STEM / "vocals.wav"
-    no_vocals = output_dir / MODEL / STEM / "no_vocals.wav"
+    vocals = output_dir / MODEL / STEM / "vocals.mp3"
+    no_vocals = output_dir / MODEL / STEM / "no_vocals.mp3"
 
     if not vocals.exists() or not no_vocals.exists():
         # 这个库没提供 APIs，暂时简单粗暴用命令行了
-        cmd = f'python -m demucs --two-stems=vocals -n {MODEL} {str(song_path)} -o {output_dir}'
+        cmd = f'python -m demucs --two-stems=vocals --mp3 --mp3-bitrate 128 -n {MODEL} {str(song_path)} -o {output_dir}'
         with locker:
             print(cmd)
             os.system(cmd)
