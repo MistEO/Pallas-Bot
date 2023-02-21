85c9fec564ede49e35c3bf5483edf6f14353a1ef
MistEO <mistereo@hotmail.com>
2023-02-21 15:26:15 +0800

    perf: 优化唱歌音频切片文件名处理

--- src/plugins/sing/slicer.py
+++ src/plugins/sing/slicer.py
@@ -8,22 +8,16 @@ def slice(path: Path, output_dir: Path, output_stem: str, format: str = 'mp3', s
     audio_segment = AudioSegment.from_file(path, format=format)
     total = int(audio_segment.duration_seconds * 1000 / size)  # 计算音频切片后的个数
 
-    exists = [output_dir /
+    results = [output_dir /
                f"{output_stem}_chunk{i}.{format}" for i in range(total + 1)]
-    if all([f.exists() for f in exists]):
-        return exists
+    if all([f.exists() for f in results]):
+        return results
 
-    results = []
     print('splitting audio...')
     for i in range(total):
-        # 将音频切片，并以顺序进行命名
-        filename = output_dir / f"{output_stem}_chunk{i}.{format}"
-        audio_segment[i * size:(i + 1) * size].export(filename, format=format)
-        results.append(filename)
+        audio_segment[i * size:(i + 1) * size].export(results[i], format=format)
 
     # 缺少结尾的音频片段
-    last_filename = output_dir / f"{output_stem}_chunk{total}.{format}"
-    audio_segment[total * size:].export(last_filename, format=format)
-    results.append(last_filename)
+    audio_segment[total * size:].export(results[-1], format=format)
 
     return results
