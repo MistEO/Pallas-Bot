09595e5d42777ddd75c160e23e7281c30e475e9d
MistEO <mistereo@hotmail.com>
2023-02-26 22:18:05 +0800

    chore: 增大主动发言间隔

--- src/plugins/repeater/__init__.py
+++ src/plugins/repeater/__init__.py
@@ -179,7 +179,7 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
         await ban_msg_latest.finish('这对角可能会不小心撞倒些家具，我会尽量小心。')
 
 
-@speak_sched.scheduled_job('interval', seconds=5)
+@speak_sched.scheduled_job('interval', seconds=60)
 async def speak_up():
 
     ret = Chat.speak()
