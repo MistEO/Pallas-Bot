ea9e08220f9a10e8ff3c0a2bc500b85c48d5b957
MistEO <mistereo@hotmail.com>
2023-02-26 17:38:12 +0800

    chore: support so_vits_svc 4.0

--- src/plugins/sing/separater.py
+++ src/plugins/sing/separater.py
@@ -16,8 +16,9 @@ def separate(song_path: Path, output_dir: Path, locker: Lock = Lock()):
 
     vocals = output_dir / MODEL / STEM / "vocals.mp3"
     no_vocals = output_dir / MODEL / STEM / "no_vocals.mp3"
+    vocals_with_stem = vocals.parent / f'{STEM}.mp3'
 
-    if not vocals.exists() or not no_vocals.exists():
+    if (not vocals_with_stem.exists() and not vocals_with_stem.exists()) or not no_vocals.exists():
         # 这个库没提供 APIs，暂时简单粗暴用命令行了
         cmd = ''
         if cuda_devices:
@@ -27,7 +28,10 @@ def separate(song_path: Path, output_dir: Path, locker: Lock = Lock()):
             print(cmd)
             os.system(cmd)
 
-    if not vocals.exists() or not no_vocals.exists():
-        return None
+        if not vocals.exists() or not no_vocals.exists():
+            return None
 
-    return vocals, no_vocals
+    if not vocals_with_stem.exists():
+        os.rename(vocals, vocals_with_stem)
+
+    return vocals_with_stem, no_vocals
