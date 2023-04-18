8baa8b015e59cf31f5d998f9d7df2959ed103b12
大虫yyc <60146099+DaChongYYC@users.noreply.github.com>
2023-04-18 19:05:31 +0800

    Update separater.py
    修复key≠0时每次唱歌都重新分离伴奏的问题

--- src/plugins/sing/separater.py
+++ src/plugins/sing/separater.py
@@ -17,10 +17,11 @@ def separate(song_path: Path, output_dir: Path, key: int = 0, locker: Lock = Loc
     STEM = song_path.stem
 
     vocals = output_dir / MODEL / STEM / "vocals.mp3"
+    no_vocals_0key = output_dir / MODEL / STEM / "no_vocals.mp3"
     no_vocals = output_dir / MODEL / STEM / f'no_vocals_{key}key.mp3'
     vocals_with_stem = vocals.parent / f'{STEM}.mp3'
 
-    if (not vocals_with_stem.exists() and not vocals_with_stem.exists()) or not no_vocals.exists():
+    if (not vocals_with_stem.exists() and not vocals_with_stem.exists()) or not no_vocals_0key.exists():
         # 这个库没提供 APIs，暂时简单粗暴用命令行了
         cmd = ''
         if cuda_devices:
@@ -32,9 +33,10 @@ def separate(song_path: Path, output_dir: Path, key: int = 0, locker: Lock = Loc
         with locker:
             print(cmd)
             os.system(cmd)
+        if not vocals.exists() or not no_vocals_0key.exists():
+            return None
 
-        no_vocals_0key = output_dir / MODEL / STEM / "no_vocals.mp3"
-
+    if  not no_vocals.exists():
         if key == 0:
             # 不调节半音，节省几秒钟
             no_vocals = no_vocals_0key
@@ -46,7 +48,7 @@ def separate(song_path: Path, output_dir: Path, key: int = 0, locker: Lock = Loc
             # 保存音调调节后的伴奏文件
             sf.write(no_vocals, y_shifted, sr)
 
-        if not vocals.exists() or not no_vocals.exists():
+        if not no_vocals.exists():
             return None
 
     if not vocals_with_stem.exists():
