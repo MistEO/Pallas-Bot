e1aa4967aad09d76dbe677fc29c2fb559e026ad7
MistEO <mistereo@hotmail.com>
2023-07-29 06:20:46 +0800

    feat: 备份脚本备份新图片缓存数据

--- tools/backup.sh
+++ tools/backup.sh
@@ -33,9 +33,7 @@ backup_important_data_in_mongodb() {
     mongodump -d PallasBot -c group_config -o $output/mongodb
     mongodump -d PallasBot -c user_config -o $output/mongodb
     mongodump -d PallasBot -c context -o $output/mongodb
-
-    mongodump -d gocq-database -c image-cache -o $output/mongodb
-    mongodump -d gocq-database -c video-cache -o $output/mongodb
+    mongodump -d PallasBot -c image_cache -o $output/mongodb
 
     ln -s $output/mongodb $output_latest/important_data_in_mongodb
 
