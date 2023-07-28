fc3974a13cc5c6d4c3eb775bc4d191912b7320ad
MistEO <mistereo@hotmail.com>
2023-07-29 06:42:22 +0800

    chore: 添加一个没什么用的清理脚本

---
+++ tools/clear_old_image.py
@@ -0,0 +1,44 @@
+import pymongo
+import time
+
+mongo_client = pymongo.MongoClient('127.0.0.1', 27017, w=0,
+        unicode_decode_error_handler='ignore')
+
+mongo_db = mongo_client['PallasBot']
+
+message_mongo = mongo_db['message']
+context_mongo = mongo_db['context']
+
+
+query = {
+          "answers.keywords": {
+            "$regex": "\\[CQ\\:image.*is_origin="
+        }}
+all_old_answers = context_mongo.find(query)
+index = 0
+for old_answers in all_old_answers:
+    new_answers = []
+    for answer in old_answers['answers']:
+        if "is_origin=" in answer['keywords']:
+            continue
+        new_answers.append(answer)
+    context_mongo.update_one({"_id": old_answers['_id']}, {"$set": {"answers": new_answers}})
+    index += 1
+    if index % 1000 == 0:
+        print(index)
+
+query = {
+    "ban.reason": "ActionFailed"
+}
+all_ban = context_mongo.find(query)
+index = 0
+for ban in all_ban:
+    new_ban = []
+    for ban_info in ban['ban']:
+        if ban_info['reason'] == "ActionFailed":
+            continue
+        new_ban.append(ban_info)
+    context_mongo.update_one({"_id": ban['_id']}, {"$set": {"ban": new_ban}})
+    index += 1
+    if index % 1000 == 0:
+        print(index)
